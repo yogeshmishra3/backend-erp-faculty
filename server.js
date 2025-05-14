ceec1434f100b5d8c67db1b42e7b362eb42a93b1
Yogesh Mishra <Yogeshrmishra@outlook.com>
2025-05-14 17:36:41 +0530

    added some changes in faculty routes

--- server.js
+++ server.js
@@ -24,15 +24,12 @@ const { errorHandler } = require("./utils/errorHandler");
 // Create Express App
 const app = express();
 
-// Middleware
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
